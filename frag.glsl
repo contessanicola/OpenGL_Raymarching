c9c11d531ca0a7fb6bead44fdd7024b15e804c4c
Contessa Nicola <47363206+contessanicola@users.noreply.github.com>
2021-09-24 13:46:53 +0200

    Add files via upload

--- frag.glsl
+++ frag.glsl
@@ -2,16 +2,19 @@
 
 #define PI 3.14159265358979
 
-#define BACKGROUND_COLOR vec3(0.53,0.8,0.95)//vec3(0.6,0.8,1.0)
-#define MAX_DIST 500.
-#define MIN_DIST 1e-6
-#define MAX_MARCHES 2000
+#define BACKGROUND_COLOR vec3(0.5,0.8,0.9)//vec3(0.6,0.8,1.0)
+#define FOG_COLOR vec3(0.30, 0.36, 0.40)
+#define LIGHT_COLOR vec3(1.0,0.9,0.7)
+#define LIGHT_DIRECTION vec3(0.36, 0.48, 0.80)
+
+#define MAX_DIST 200.
+#define MIN_DIST 1e-5
+#define MAX_MARCHES 1000
 #define SUN_SIZE 0.001
-#define SUN_SHARPNESS 2.0
-#define FOCAL_DIST 1.73205080757
+#define SUN_SHARPNESS 1.5
 #define POWER 8
-#define OSCILLATION 0
-#define AA 0
+#define OSCILLATION 1
+#define AA 1
 
 
 in vec4 gl_FragCoord;
@@ -25,8 +28,7 @@ uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
 
-const vec3 LIGHT_COLOR = vec3(1.0,0.95,0.8);
-const vec3 LIGHT_DIRECTION = vec3(0.36, 0.48, 0.80);
+
 float res;
 
 // https://github.com/HackerPoet/MarbleMarcher/blob/master/assets/frag.glsl
@@ -169,11 +171,11 @@ float sdBox(vec3 p, vec3 b){
 }
 
 float sdMenger(vec3 p){
-    float size=20.;
+    float size=10.;
 	p.z -=3.; 
     vec3[] s = vec3[](vec3(1,1,1),vec3(1,1,0));
     
-    for(int iter=0;iter<15;++iter){
+    for(int iter=0;iter<10;++iter){
        
         p=abs(p);
         if(p.y > p.x) p.yx = p.xy;
@@ -199,11 +201,11 @@ float sminCubic( float a, float b, float k ){
 
 float distanceField(vec3 p){
     
-    //float Sphere = sdSphere(p-vec3(0.0,3.0,0.0),2.0);
+    //float Sphere = sdSphere(p-vec3(0.0,0.0,0.0),2.0);
     //float Box = sdBox(p,vec3(2.0));
     //float Torus = sdTorus(p,vec2(2.0,0.5));
     //float SphereMod = sdSphereMod(p,2.0);
-    //float Plane = sdPlane(p);
+    //float Plane = sdPlane(p-vec3(0.0,-2.0,0.0));
     
     //float Menger = sdMenger(p);
     //float Mandelbulb2 = sdMandelbulb2(p);
@@ -211,14 +213,14 @@ float distanceField(vec3 p){
     //float MandelBox2 = mandelBox2(p).x;
     //return sminCubic(Sphere, Torus,0.5);
     //return max(Torus,SphereMod);
-    //return min(Box,Mandelbulb);
+    //return min(Sphere,Plane);
     
-    float Box = sdBox(p,vec3(1.25));
-    //if(Box > 0.01) return Box;
+    float Box = sdBox(p,vec3(1.5));
+    if(Box > 0.1) return Box;
     float Mandelbulb = sdMandelbulb(p);
     return Mandelbulb;
 
-    //return Menger;
+    //return SphereMod;
 }
 
 vec3 calcNormal(vec3 p, float h){ // https://www.iquilezles.org/www/articles/normalsSDF/normalsSDF.htm
@@ -230,12 +232,12 @@ vec3 calcNormal(vec3 p, float h){ // https://www.iquilezles.org/www/articles/nor
 }
 
 //TO WORK THE BEST THIS NEED A PROPER DISTANCE FIELD COULD CAUSE ARTIFACTS WITH MIN()
-float softShadow(in vec3 ro, in vec3 rd, float mint, float maxt, float k){ //https://www.iquilezles.org/www/articles/rmshadows/rmshadows.htm
+float softShadow(in vec3 ro, in vec3 rd, float mint, float maxt, float k, float min_dist){ //https://www.iquilezles.org/www/articles/rmshadows/rmshadows.htm
     float res = 1.0;
     float ph = 1e10;
     for(float t=mint; t<maxt;){
         float h = distanceField(ro + rd*t);
-        if( h < 0.001)
+        if( h < min_dist)
             return 0.0;
         float y = h*h/(2.0*ph);
         float d = sqrt(h*h-y*y);
@@ -245,99 +247,114 @@ float softShadow(in vec3 ro, in vec3 rd, float mint, float maxt, float k){ //htt
     }
     res = clamp( res, 0.0, 1.0 );
     return res*res*(3.0-2.0*res);
+    //return res;
 }
 
 float ambientOcclusion(vec3 p, vec3 n){
-    float sum    = 0;
-    float maxSum = 0;
-    for (int i = 0; i < 3; i ++)
-    {
-        vec3 p = p + n * (i+1) * 0.2;
-        sum    += 1. / pow(2., i) * distanceField(p);
-        maxSum += 1. / pow(2., i) * (i+1) * 0.2;
+    float steps = 0.1;
+    float ao = 0.0;
+    float dist;
+    for(int i=1; i<= 3;i++){
+        dist = steps * i;
+        ao += max(0.0,(dist - distanceField(p+n*dist))/ dist);
     }
-    return sum / maxSum;
+    return (1.0-ao * 0.22);
 }
 
-vec3 raymarching(vec3 ro, vec3 rd){
+
+vec3 raymarching(vec3 ro, vec3 rd, out int iter){
     float min_d = 1.0;
     float t = 0; //distance traveled alongside the ray vector
+    float s = 0;
     float d = 0;
-    for(int i = 0; i < MAX_MARCHES; i++){       
+    for(int i = 0; i < MAX_MARCHES; i++){   
+        iter = i;    
         float min_dist = max(res*t, MIN_DIST);
-        float min_d = min(min_d, 10.0 * d / t);
-
+        //float min_d = min(min_d, 10.0 * d / t); Check how close we got without hitting so we can use it for something eg GLOW EFFECT
         d = distanceField(ro + rd * t);
 
         if (t > MAX_DIST){ break; }
-        else if (d < min_dist){ break; }
-
+        else if (d < min_dist){
+            s += d / min_dist;         // can use this for fake ambient occlusion
+            break;
+        }     
         t += d;      
     }
-    return vec3(d,t,min_d);
+    return vec3(d,t,s);
 } 
 
 vec4 render(vec3 ro, vec3 rd){
     vec4 col = vec4(0.0);
-    vec3 raymarch = raymarching(ro,rd);
+    int iter;
+    vec3 raymarch = raymarching(ro,rd,iter);
     float d = raymarch.x;
     float t = raymarch.y;
-    float min_d = raymarch.z;
+    float s = raymarch.z;
 
     float min_dist = max(res*t, MIN_DIST);
     vec3 p = ro + rd * t;
     if(d < min_dist){
-            vec3 n = calcNormal(p,min_dist);
+        vec3 n = calcNormal(p,min_dist);
 
-            //ro = ro - n*d;
+        vec3 sun_light = LIGHT_COLOR * clamp(dot(n,LIGHT_DIRECTION ), 0., 1.);//LIGHT_COLOR * max(dot(n, LIGHT_DIRECTION), 0.0);
+        vec3 sky_light = (BACKGROUND_COLOR*0.10)* clamp(0.5+0.5*dot(n,vec3(0.,1.,0.)), 0., 1.);
+        vec3 bounce_light = (vec3(.06,.063,.07))* clamp(0.5+0.5*dot(n,vec3(0.,-1.,0.)), 0., 1.);
+        //vec3 bounce_light = (LIGHT_COLOR*0.015)* sqrt(clamp( 0.1-0.9*n.y, 0.0, 1.0 ))*clamp(1.0-0.1*p.y,0.0,1.0);
+        vec3 sun_shadow = LIGHT_COLOR * softShadow(p+n*min_dist, LIGHT_DIRECTION, 0.01, 64.0, 12.0, min_dist);
+        
+        // IF YOU HAVE A PLANE IT SHOULD TECNICALLY BOUNCE SOME LIGHT FROM THE SUN UNDER YOUR OBJECT, THIS LIGHT SHOULD
+        // COME FROM THE BOTTOM dot(n,vec3(0.0,-1.0,0.0)) AND IT SHOULD BE THE COLOR OF THE SUN * THE PLANE OR BOTTOM LIGHT
 
-            float light =  0.35 * (dot(n, LIGHT_DIRECTION) - 1.0) + 1.0;
-            light = max(light, 0.3);
-            float ao = ambientOcclusion(p,n)*0.2+0.5;
-            vec3 lightdir = p;
-            lightdir += n * MIN_DIST * 100;
-            float shadow = softShadow(lightdir, LIGHT_DIRECTION, 0.01, 32.0, 8.0)*0.25+0.25;
-
-            float k = min_d * min(t, 1.0);
-            vec3 reflected = n - 2.0*dot(n, n) * n;
-            float specular = max(dot(reflected, LIGHT_DIRECTION), 0.0);
-            specular = pow(specular, 40);   
-            
-            col.xyz +=  LIGHT_COLOR * specular * (k*0.25); //LIGHT COLOR * SPECULAR INTENSITY * SPECULAR FACTOR
-            col.xyz += shadow * light * ao ;
-            
+        float ao = ambientOcclusion(p,n);
+        
+        /*float k = min_d * min(t, 1.0);
+        vec3 reflected = n - 2.0 * dot(n,n) * n;
+        float specular = max(dot(reflected, LIGHT_DIRECTION), 0.0);
+        specular = pow(specular, 40);   
+        
+        //col.xyz += LIGHT_COLOR * specular * (0.25); //LIGHT COLOR * SPECULAR INTENSITY * SPECULAR FACTOR*/
+        col.xyz += sun_light * sun_shadow;        
+        col.xyz += sky_light * ao;   
+        col.xyz += bounce_light * ao;
     }
     else {
-        col.xyz += BACKGROUND_COLOR;
-        float sun_spec = dot(rd, LIGHT_DIRECTION) - 1.0 + SUN_SIZE;
-        sun_spec = min(exp(sun_spec * SUN_SHARPNESS / SUN_SIZE), 1.0);
-        col.xyz += LIGHT_COLOR * sun_spec;
+        vec3 sky = BACKGROUND_COLOR - max(rd.y,0.0)*0.5;
+        float sun = dot(rd, LIGHT_DIRECTION) - 1.0 + SUN_SIZE;
+        sun = min(exp(sun * SUN_SHARPNESS / SUN_SIZE), 1.0);
+        col.xyz += sky;
+        col.xyz += LIGHT_COLOR * sun;
+        //float fogAmount = 1.0 - exp(-(p.z-8.0) * (1.0/MAX_DIST));
+        //col.xyz += mix(col.xyz, FOG_COLOR, fogAmount);
     }
+    //return vec4(vec3(float(iter)/128.0), 1.0); //check ray iteration
     return col;
 } 
 
 void main()
 {   
+    vec4 tot;
     res = 1.0 / 2160.0;
     vec4 col = vec4(0.0);
-   // for (int i = 0; i < AA; ++i) {
-	//	for (int j = 0; j < AA; ++j) {
-            //vec2 aa = vec2(i,j)/AA - 0.5;
-            vec2 uv = (gl_FragCoord.xy+0.5*(-iResolution.xy))/iResolution.y;
-            //vec2 uv = (-iResolution.xy + 2.0*(gl_FragCoord.xy+aa))/iResolution.y;
+    for (int i = 0; i < AA; ++i) {
+		for (int j = 0; j < AA; ++j) {
+            vec2 aa =-0.5+vec2(i,j)/AA;
+            //vec2 uv = (gl_FragCoord.xy+0.5*(-iResolution.xy))/iResolution.y;
+            vec2 uv = (((gl_FragCoord.xy+aa)-0.5*iResolution.xy))/iResolution.y; //vec2 uv = (-iResolution.xy + (2*gl_FragCoord.xy+aa))/iResolution.y;
+            //AA cause object to distort on the edge of the screen to compensate you increase the FOCAL DISTANCE
+           
+            vec4 far_4 = inverse(view) * vec4(normalize(vec3(uv.x,uv.y,-1.41)), 1.0);
 
-            vec3 lookAt = normalize(vec3(uv.x,uv.y,-1));
-            vec4 lookAt4 = vec4(lookAt, 1.0);
+            vec3 far_3 = far_4.xyz/far_4.w;
 
-            vec4 far_4 = inverse(view) * lookAt4;
+            vec3 rd = normalize(far_3.xyz - cameraPos);
 
-            vec3 far3 = far_4.xyz/far_4.w;
-
-            vec3 rd = normalize(far3 - cameraPos);
-
+            //col.xyz =  vec3(length(uv),length(uv),length(uv));
             col = render(cameraPos,rd);
-    //    }
-    //}
-    //col /= AA*AA;
-    FragColor = col;
+
+            col = pow(col, vec4(0.4545)); //GAMMA CORRECTION
+            tot += col;                   
+        }
+    }
+    tot /= AA*AA;
+    FragColor = tot;
 }
